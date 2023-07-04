d94f0374029f880c5ca8598821d016b636918fe4
Vladyslav Koshelenko <Vladyslav_Koshelenko@epam.com>
2023-07-04 13:00:18 +0300

    add account model

--- blockchain/main.py
+++ blockchain/main.py
@@ -5,36 +5,16 @@ from Block import Block
 from Blockchain import Blockchain
 from BlockchainUtils import BlockchainUtils
 import pprint
+from AccountModel import AccountModel
 if __name__ == '__main__':
 
-    sender = 'sender'
-    receiver = 'receiver'
-    amount = 1
-    type = 'TRANSFER'
-
     wallet = Wallet()
-    anotherwallet = Wallet()
-    pool = TransactionPool()
+    accountModel = AccountModel()
 
-    transaction = wallet.createTransaction(receiver,amount,type)
-  
-    if pool.transactionExists(transaction) == False:
-        pool.addTransaction(transaction)
-    
-    blockchain = Blockchain()
-    lastHash =  BlockchainUtils.hash(blockchain.blocks[-1].payload()).hexdigest()
-    blockCount = blockchain.blocks[-1].blockCount + 1
-    block = wallet.createBlock(pool.transactions, lastHash, blockCount)
+    accountModel.updateBalace(wallet.publicKeyString(), 10)
+    accountModel.updateBalace(wallet.publicKeyString(), -5)
 
-
-    if not blockchain.lashBlockHashValid(block):
-        print('LastBlockHash is not valid')
-    if not blockchain.blockCountValid(block):
-        print('BlockCount is not valid')
-
-    if blockchain.lashBlockHashValid(block) and blockchain.blockCountValid(block):
-        blockchain.addBlock(block)
-    pprint.pprint(blockchain.toJson())
+    print(accountModel.balances)
     
 
 
